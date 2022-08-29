37ad5c17bdc9784cb86563c8461eb4c246cd80c9
Eze Ezinne Leticia <ezeezinn42@gmail.com>
2022-08-29 09:27:13 +0100

    changed app.js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Hello World");
-console.log("what the uck is going on?");
+console.log(
+  "what the uck is going on? ohhhhh just forget it, i have already fixed the bug through christ my lord"
+);
